d4f0d553430f600566b79822f0177882ff3fd543
rahaf19-meet <rahaf19@meet.mit.edu>
2017-07-24 16:44:30 +0300

    Stopping here

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+side_length=200
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(0,side_length)
+turtle.goto(side_length,side_length)
+turtle.goto(side_length,0)
+turtle.goto(0,0)
+turtle.mainloop()
+
+
